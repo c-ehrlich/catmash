2037918e42db5b48ecb1e9a5d38ce7c8f44b8c24
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-26 18:44:23 +0200

    Remove Name from CatPic table

---
+++ prisma/migrations/20220626155401_remove_cat_name/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `CatPic` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CatPic" DROP COLUMN "name";
